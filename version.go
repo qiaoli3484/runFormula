3d77e1b283e58e482d9b890a8ec0d448079dea19
qlmm <574168030@qq.com>
2021-12-27 07:21:51 +0000

    add version.go.

---
+++ version.go
@@ -0,0 +1,7 @@
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package runformula
+
+// Version is the current gin framework's version.
+const Version = "v1.0.0"
